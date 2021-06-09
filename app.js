acd6c7e52c8035b573eaeeb58afea1f6a4cf7e0b
{shunsato4888@gmail.com} <shunsato4888@gmail.com>
2021-06-09 22:47:18 +0900

    ちょっと修正

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 let budget = prompt('所持金を入力してください');
 budget = parseFloat(budget);
-if (budget >= 1500) {
+
+let isHungry = confirm('君、お腹すいてるん？');
+
+if (budget >= 1500 && isHungry) {
     alert('ピザを届けてやるよ。');
+} else if (budget >= 500) {
+    alert('金が無いならポテトを食いな。');
+} else {
+    alert('貴様に与える食いもんは無ぇ');
 }
